e5e5362566ad3fd1bda430f38b8a34e447589ecd
Sarah Monthofer <44783556+monthofer@users.noreply.github.com>
2018-11-14 18:54:40 -0800

    Add files via upload

--- username.sh
+++ username.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 # zip.sh
-echo "Enter a five-digit ZIP code: "
+echo "Enter your username: "
 read ZIP
 while echo $ZIP | egrep -v "^[0-9]{3-12}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
+	echo "Choose your username between 3 and 12 characters"
 	echo "You can only enter lower case letters, numbers, and underscore"
-	echo "Enter a five-digit ZIP code: "
+	echo "Enter your username "
 	read ZIP
 	echo ${password:0:1}
 	case ${password:0:1} in
